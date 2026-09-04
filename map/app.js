dd800d1ba1939e6c06d21a3e319241ab3d981343
David Passmore <davidpassmore@gmail.com>
2026-09-04 11:46:43 -0400

    change map tile

--- map/app.js
+++ map/app.js
@@ -2,9 +2,10 @@
 const map = L.map('map').setView([0, 0], 2);
 
 // 2. Load OpenStreetMap raster tiles
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+  maxZoom: 20,
+  subdomains: 'abcd',
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
 }).addTo(map);
 
 // 3. Request user location
